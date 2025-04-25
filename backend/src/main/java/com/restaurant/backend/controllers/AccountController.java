92550e0eb3a2221d7d2baa8077742bb7d5178bbf
Khong <hankhongg@gmail.com>
2025-04-25 14:22:41 +0700

    refactor: account related

--- backend/src/main/java/com/restaurant/backend/controllers/AccountController.java
+++ backend/src/main/java/com/restaurant/backend/controllers/AccountController.java
@@ -4,9 +4,7 @@ import com.restaurant.backend.domains.dto.Account.AccountDto;
 import com.restaurant.backend.domains.dto.Account.dto.ForgotPasswordDto;
 import com.restaurant.backend.domains.dto.Account.dto.UpdateAccountDto;
 import com.restaurant.backend.domains.dto.Account.dto.VerifyDto;
-import com.restaurant.backend.domains.entities.Account;
 import com.restaurant.backend.domains.dto.Account.dto.LoginDto;
-import com.restaurant.backend.mappers.impl.AccountMapper;
 import com.restaurant.backend.services.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,86 +13,43 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AccountController {
     private final AccountService accountService;
-    private final AccountMapper accountMapper;
-    public AccountController(AccountService accountService, AccountMapper accountMapper) {
+
+    public AccountController(AccountService accountService) {
         this.accountService = accountService;
-        this.accountMapper = accountMapper;
     }
 
-    @PostMapping(path = "/signup") // may not use it ever
-    public ResponseEntity<AccountDto> login(@RequestBody AccountDto accountDto) {
-        Account account = accountMapper.mapTo(accountDto);
-        Account savedAccount = this.accountService.save(account);
-        return new ResponseEntity<>(accountMapper.mapFrom(savedAccount), HttpStatus.OK); // not showing body properly
+    @PostMapping(path = "/signup")
+    public ResponseEntity<AccountDto> signup(@RequestBody AccountDto accountDto) {
+        AccountDto savedDto = accountService.signup(accountDto);
+        return new ResponseEntity<>(savedDto, HttpStatus.OK);
     }
 
     @PostMapping(path = "/login")
     public ResponseEntity<AccountDto> login(@RequestBody LoginDto loginDto) {
-        AccountDto dbAccountDto = this.accountMapper.mapFrom(this.accountService.checkLogin(loginDto));
-        return this.accountService.checkLogin(loginDto) != null ? new ResponseEntity<>(dbAccountDto,HttpStatus.OK) : new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        AccountDto dbAccountDto = accountService.login(loginDto);
+        return dbAccountDto != null ? new ResponseEntity<>(dbAccountDto, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
-    @PostMapping(path= "/forgot-password")
+    @PostMapping(path = "/forgot-password")
     public ResponseEntity<String> forgotPassword(@RequestBody ForgotPasswordDto forgotPasswordDto) {
-        try {
-            Account foundAccount = this.accountService.getAccountByUsername(forgotPasswordDto.getAccUsername());
-            if(foundAccount != null) {
-                String sentCode = this.accountService.sendVerificationCode(foundAccount.getAccEmail());
-                return new ResponseEntity<>(HttpStatus.OK);
-            }
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-
-        }
-        catch (Exception e) {
-            System.out.println(e);
-        }
-        return null;
+        boolean success = accountService.forgotPassword(forgotPasswordDto);
+        return success ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
-    @PostMapping(path="/verify")
+    @PostMapping(path = "/verify")
     public boolean verify(@RequestBody VerifyDto verifyDto) {
-        return this.accountService.verify(verifyDto.getAccEmail(), verifyDto.getCode());
+        return accountService.verify(verifyDto.getAccEmail(), verifyDto.getCode());
     }
 
-    @PutMapping(path="/update-account/{username}")
+    @PutMapping(path = "/update-account/{username}")
     public ResponseEntity<AccountDto> updateAccount(@RequestBody UpdateAccountDto updateAccountDto, @PathVariable String username) {
-        Account account = accountMapper.mapTo(updateAccountDto);
-        account.setAccUsername(username);
-        Account savedAccount = this.accountService.save(account);
-        return new ResponseEntity<>(accountMapper.mapFrom(savedAccount), HttpStatus.OK);
+        AccountDto updated = accountService.updateAccount(updateAccountDto, username);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
-    @PatchMapping(path="/update-account/{username}")
+    @PatchMapping(path = "/update-account/{username}")
     public ResponseEntity<AccountDto> partialUpdateAccount(@RequestBody UpdateAccountDto updateAccountDto, @PathVariable String username) {
-        Account account = accountMapper.mapTo(updateAccountDto);
-        Account dbAccount = this.accountService.getAccountByUsername(username);
-
-        if (account.getAccEmail() != null) {
-            dbAccount.setAccEmail(account.getAccEmail());
-        }
-        if (account.getAccPhone() != null) {
-            dbAccount.setAccPhone(account.getAccPhone());
-        }
-        if (account.getAccGender() != null) {
-            dbAccount.setAccGender(account.getAccGender());
-        }
-        if (account.getAccBday() != null) {
-            dbAccount.setAccBday(account.getAccBday());
-        }
-        if (account.getAccAddress() != null) {
-            dbAccount.setAccAddress(account.getAccAddress());
-        }
-        if (account.getAccPassword() != null) {
-            dbAccount.setAccPassword(account.getAccPassword());
-        }
-        if (account.getRole() != null) {
-            dbAccount.setRole(account.getRole());
-        }
-        if(account.getAccDisplayname() != null) {
-            dbAccount.setAccDisplayname(account.getAccDisplayname());
-        }
-
-        Account savedAccount = this.accountService.save(dbAccount);
-        return new ResponseEntity<>(accountMapper.mapFrom(savedAccount), HttpStatus.OK);
+        AccountDto updated = accountService.partialUpdateAccount(updateAccountDto, username);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/restaurant/backend/services/impl/AccountServiceImpl.java
+++ backend/src/main/java/com/restaurant/backend/services/impl/AccountServiceImpl.java
@@ -1,8 +1,13 @@
 package com.restaurant.backend.services.impl;
 
+import com.restaurant.backend.domains.dto.Account.AccountDto;
+import com.restaurant.backend.domains.dto.Account.dto.ForgotPasswordDto;
+import com.restaurant.backend.domains.dto.Account.dto.UpdateAccountDto;
 import com.restaurant.backend.domains.entities.Account;
 import com.restaurant.backend.domains.entities.AccountRole;
 import com.restaurant.backend.domains.dto.Account.dto.LoginDto;
+import com.restaurant.backend.mappers.impl.AccountMapper;
+import com.restaurant.backend.mappers.impl.AccountRoleMapper;
 import com.restaurant.backend.other_services.EmailService;
 import com.restaurant.backend.repositories.AccountRepository;
 import com.restaurant.backend.repositories.AccountRoleRepository;
@@ -23,17 +28,30 @@ public class AccountServiceImpl implements AccountService {
     private final PasswordEncoder passwordEncoder;
     @Autowired
     private final EmailService emailService;
+    private final AccountMapper accountMapper;
+    private final AccountRoleMapper accountRoleMapper;
 
     private final Map<String, String> verificationCodes = new HashMap<>();
 
-    public AccountServiceImpl(AccountRepository accountRepository, AccountRoleRepository accountRoleRepository, PasswordEncoder passwordEncoder, EmailService emailService) {
+    public AccountServiceImpl(
+            AccountRepository accountRepository,
+            AccountRoleRepository accountRoleRepository,
+            PasswordEncoder passwordEncoder,
+            EmailService emailService,
+            AccountMapper accountMapper,
+            AccountRoleMapper accountRoleMapper
+    ) {
         this.accountRepository = accountRepository;
         this.accountRoleRepository = accountRoleRepository;
         this.passwordEncoder = passwordEncoder;
         this.emailService = emailService;
+        this.accountMapper = accountMapper;
+        this.accountRoleMapper = accountRoleMapper;
     }
 
-    public Account save(Account account) {
+    @Override
+    public AccountDto signup(AccountDto accountDto) {
+        Account account = accountMapper.mapTo(accountDto);
         AccountRole accountRole = accountRoleRepository.findByRoleName(account.getRole().getRoleName());
         if (accountRole == null) {
             accountRole = new AccountRole();
@@ -41,50 +59,71 @@ public class AccountServiceImpl implements AccountService {
             accountRole = this.accountRoleRepository.save(accountRole);
         }
         account.setRole(accountRole);
-
-        String hashedPassword = passwordEncoder.encode(account.getAccPassword());
-        account.setAccPassword(hashedPassword);
-        return accountRepository.save(account);
-    }
-    public Account checkLogin(LoginDto loginDto) {
-        Optional<Account> foundAccount = this.accountRepository.findOneByAccUsername(loginDto.getAccUsername());
-        String hashedPassword = passwordEncoder.encode(loginDto.getAccPassword());
-        System.out.println(foundAccount);
-        if (foundAccount.isPresent()) {
-            if (passwordEncoder.matches(
-                    loginDto.getAccPassword(),
-                    foundAccount.get().getAccPassword()
-            )){
-                return foundAccount.get();
-            }
-        }
-        return null;
-    }
-    public Account getAccountByUsername(String username){
-        Optional<Account> foundAccount = this.accountRepository.findOneByAccUsername(username);
-        return foundAccount.orElse(null);
+        account.setAccPassword(passwordEncoder.encode(account.getAccPassword()));
+        Account saved = accountRepository.save(account);
+        return accountMapper.mapFrom(saved);
     }
 
-    public Account getAccountByEmail(String email){
-        Optional<Account> foundAccount = this.accountRepository.findOneByAccEmail(email);
-        return foundAccount.orElse(null);
-    }
-
-    public String sendVerificationCode(String email) {
-        Optional<Account> foundAccount = this.accountRepository.findOneByAccEmail(email);
-        Random random = new Random();
-        String code = String.valueOf(100000 + random.nextInt(899999));
-        if(foundAccount.isPresent()) {
-            emailService.sendVerificationCode(email, code);
-            verificationCodes.put(email, code);
-            return code;
+    @Override
+    public AccountDto login(LoginDto loginDto) {
+        Optional<Account> found = accountRepository.findOneByAccUsername(loginDto.getAccUsername());
+        if (found.isPresent() && passwordEncoder.matches(loginDto.getAccPassword(), found.get().getAccPassword())) {
+            return accountMapper.mapFrom(found.get());
         }
         return null;
     }
 
+    @Override
+    public boolean forgotPassword(ForgotPasswordDto dto) {
+        Optional<Account> found = accountRepository.findOneByAccUsername(dto.getAccUsername());
+        if (found.isPresent()) {
+            String code = String.valueOf(100000 + new Random().nextInt(899999));
+            emailService.sendVerificationCode(found.get().getAccEmail(), code);
+            verificationCodes.put(found.get().getAccEmail(), code);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
     public boolean verify(String email, String code) {
         return code.equals(verificationCodes.get(email));
     }
 
+    @Override
+    public AccountDto updateAccount(UpdateAccountDto updateDto, String username) {
+        Account account = accountMapper.mapTo(updateDto);
+        account.setAccUsername(username);
+        AccountRole role = accountRoleRepository.findByRoleName(account.getRole().getRoleName());
+        if (role == null) {
+            role = accountRoleRepository.save(account.getRole());
+        }
+        account.setRole(role);
+        account.setAccPassword(passwordEncoder.encode(account.getAccPassword()));
+        return accountMapper.mapFrom(accountRepository.save(account));
+    }
 
+    @Override
+    public AccountDto partialUpdateAccount(UpdateAccountDto updateDto, String username) {
+        Account dbAccount = accountRepository.findOneByAccUsername(username).orElseThrow();
+
+        if (updateDto.getAccEmail() != null) dbAccount.setAccEmail(updateDto.getAccEmail());
+        if (updateDto.getAccPhone() != null) dbAccount.setAccPhone(updateDto.getAccPhone());
+        if (updateDto.getAccGender() != null) dbAccount.setAccGender(updateDto.getAccGender());
+        if (updateDto.getAccBday() != null) dbAccount.setAccBday(updateDto.getAccBday());
+        if (updateDto.getAccAddress() != null) dbAccount.setAccAddress(updateDto.getAccAddress());
+        if (updateDto.getAccPassword() != null) dbAccount.setAccPassword(passwordEncoder.encode(updateDto.getAccPassword()));
+        if (updateDto.getRole() != null) {
+            AccountRole role = accountRoleRepository.findByRoleName(updateDto.getRole().getRoleName());
+            if (role == null) {
+                role = accountRoleRepository.save(this.accountRoleMapper.mapTo(updateDto.getRole()));
+
+            }
+            dbAccount.setRole(role);
+        }
+        if (updateDto.getAccDisplayname() != null) dbAccount.setAccDisplayname(updateDto.getAccDisplayname());
+
+        return accountMapper.mapFrom(accountRepository.save(dbAccount));
+    }
 }
+
