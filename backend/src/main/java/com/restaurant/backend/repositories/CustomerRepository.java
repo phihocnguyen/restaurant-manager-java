6796081a21db48e3ad2d1ef03c410df65c9594b4
ch1ta <ng.phihoc123@gmail.com>
2025-04-27 13:38:46 +0700

    update(customer): add more repository and service file

--- backend/src/main/java/com/restaurant/backend/repositories/CustomerRepository.java
+++ backend/src/main/java/com/restaurant/backend/repositories/CustomerRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    boolean existsByCccd(String cccd);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
 }
