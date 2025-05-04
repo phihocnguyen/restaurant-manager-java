701ddf935223c5d4bd8c5f0425ce0c95c08a52b3
korobe <156888883+korobe0906@users.noreply.github.com>
2025-05-04 19:04:37 +0700

    feat:manager_options

--- frontend/src/main/Main.java
+++ frontend/src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public static final String CURRENCY = "$";
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/manager_bookings.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/manager_options.fxml"));
 
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
