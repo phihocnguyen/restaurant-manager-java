3e5a11b2fa204535e90b4760715d30cc2f261efc
korobe <156888883+korobe0906@users.noreply.github.com>
2025-05-04 16:37:57 +0700

    fix:ingredient

--- frontend/src/Controller/ManagerIngredient.java
+++ frontend/src/Controller/ManagerIngredient.java
@@ -1,4 +1,15 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class ManagerIngredient {
+
+    @FXML
+    private TableView<?> ingredientView;
+
+    @FXML
+    private AnchorPane root;
+
 }
--- frontend/src/Controller/SidebarController.java
+++ frontend/src/Controller/SidebarController.java
@@ -14,6 +14,8 @@ public class SidebarController {
     @FXML private HBox boxReceipt;
     @FXML private HBox boxCus;
     @FXML private HBox boxStaff;
+    @FXML private HBox boxIngre;
+
     @FXML
     public void initialize() {
         // Mặc định chọn Tổng quan lúc đầu
@@ -61,5 +63,10 @@ public class SidebarController {
     public void handleStaffClick(MouseEvent mouseEvent) {
         setSelected(boxStaff);
         loadViewToContentArea("/views/manager_staff.fxml");
+    }
+    @FXML
+    public void handleIngreClick(MouseEvent mouseEvent) {
+        setSelected(boxIngre);
+        loadViewToContentArea("/views/manager_ingredient.fxml");
     }
 }
