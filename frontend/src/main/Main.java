89066079a40a6ff574d2718f0164e371059706f1
korobe <156888883+korobe0906@users.noreply.github.com>
2025-05-04 18:33:36 +0700

    fix:managerbookings

--- frontend/src/main/Main.java
+++ frontend/src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public static final String CURRENCY = "$";
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/manager_bills.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/manager_bookings.fxml"));
 
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
