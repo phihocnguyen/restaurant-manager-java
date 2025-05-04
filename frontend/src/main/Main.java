46be71b1b6fab7dce08ec96cb26785e43689a879
korobe <156888883+korobe0906@users.noreply.github.com>
2025-05-04 20:19:57 +0700

    fix:recipeui

--- frontend/src/main/Main.java
+++ frontend/src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public static final String CURRENCY = "$";
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/ingredient_add.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/recipe_add.fxml"));
 
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
