9f0ed598282a8fd6af3bca320b8e581f5f8fbfc9
Nguyen Truong Ngoc Han <23520434@gm.uit.edu.vn>
2025-05-13 09:04:05 +0700

    feat: implement login interface

--- frontend/src/Controller/ReceiptDetail.java
+++ frontend/src/Controller/ReceiptDetail.java
@@ -1,4 +1,19 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 public class ReceiptDetail {
+
+    @FXML
+    private ImageView exitButton;
+
+    @FXML
+    private void onExitButtonClick() {
+
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        stage.close();
+
+    }
 }
